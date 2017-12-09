11f93b4986b3a815490a221f0cfc9a68c5db1c46
kokushin <mail@kokush.in>
2017-12-09 21:42:31 +0900

    fix style

--- js/script.js
+++ js/script.js
@@ -1,111 +1,120 @@
-'use strict'
+'use strict';
 
-window.addEventListener('load', () => {
-  createList()
-  createTweetButton()
-})
+class Atoyomi {
 
-document.getElementById('save').addEventListener('click', (e) => {
-  e.preventDefault()
-  savePage()
-})
+  constructor () {
+    this.bind()
+    this.createList()
+    this.createTweetButton()
+  }
 
-const savePage = () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    const data = {
-      title: tab.title,
-      url: tab.url
+  bind () {
+    document.getElementById('save').addEventListener('click', (events) => {
+      events.preventDefault()
+      this.savePage()
+    })
+  }
+
+  savePage () {
+    chrome.tabs.getSelected(null, (tab) => {
+      const data = {
+        title: tab.title,
+        url: tab.url
+      }
+      this.setDataStorage(data)
+    })
+  }
+
+  setDataStorage (data) {
+    const schema = {
+      id: this.uniqueId(),
+      title: data.title,
+      url: data.url
     }
 
-    setDataStorage(data)
-  })
-}
+    let values = this.getDataStorage()
 
-const setDataStorage = (data) => {
-  const schema = {
-    id: uniqueId(),
-    title: data.title,
-    url: data.url
+    if (values !== null) {
+      values.unshift(schema)
+    } else {
+      values = []
+      values.push(schema)
+    }
+
+    localStorage.setItem('data', JSON.stringify(values))
+
+    this.createList({
+      refresh: true
+    })
   }
 
-  let values = getDataStorage()
-
-  if (values !== null) {
-    values.unshift(schema)
-  } else {
-    values = []
-    values.push(schema)
+  getDataStorage () {
+    return JSON.parse(localStorage.getItem('data'))
   }
 
-  localStorage.setItem('data', JSON.stringify(values))
+  deleteDataStorage (id) {
+    let values = this.getDataStorage()
 
-  createList({
-    refresh: true
-  })
-}
+    for (let i = 0; i < values.length; i++) {
+      if (values[i].id === id) {
+        values.splice(i, 1)
+      }
+    }
 
-const getDataStorage = () => {
-  return JSON.parse(localStorage.getItem('data'))
-}
+    localStorage.setItem('data', JSON.stringify(values))
 
-const deleteDataStorage = (id) => {
-  let values = getDataStorage()
+    this.createList({
+      refresh: true
+    })
+  }
 
-  for (let i = 0; i < values.length; i++) {
-    if (values[i].id === id) {
-      values.splice(i, 1)
+  createList (option = false) {
+    const $list = document.getElementById('list')
+    const items = this.getDataStorage()
+
+    if (option && option.refresh) {
+      $list.innerHTML = ''
+    }
+
+    if (items.length > 0) {
+      for (let key in items) {
+        const listItem = document.createElement('li')
+
+        listItem.innerHTML = `<a href="${items[key].url}" data-id="${items[key].id}">${items[key].title}</a>`
+        listItem.addEventListener('click', (events) => {
+          this.openNewTab(events)
+        })
+
+        $list.appendChild(listItem)
+      }
+    } else {
+      $list.insertAdjacentHTML('beforeend', '<li class="is-empty">「あとで読む」ページはありません</li>')
     }
   }
 
-  localStorage.setItem('data', JSON.stringify(values))
-
-  createList({
-    refresh: true
-  })
-}
-
-const createList = (option = false) => {
-  const $list = document.getElementById('list')
-  const items = getDataStorage()
-
-  if (option && option.refresh) {
-    $list.innerHTML = ''
+  createTweetButton () {
+    chrome.tabs.getSelected(null, (tab) => {
+      document.getElementById('tweet').setAttribute('href', `https://twitter.com/intent/tweet?url=${encodeURIComponent(tab.url)}&text=あとで読む+%2F+${encodeURIComponent(tab.title)}`)
+    })
   }
 
-  if (items.length > 0) {
-    for (let key in items) {
-      const listItem = document.createElement('li')
+  openNewTab (events) {
+    events.preventDefault()
 
-      listItem.innerHTML = `<a href="${items[key].url}" data-id="${items[key].id}">${items[key].title}</a>`
-      listItem.addEventListener('click', openNewTab)
+    const id = events.currentTarget.children[0].dataset.id
+    const url = events.currentTarget.children[0].getAttribute('href')
 
-      $list.appendChild(listItem)
+    window.open(url)
+
+    this.deleteDataStorage(id)
+  }
+
+  uniqueId () {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
     }
-  } else {
-    $list.insertAdjacentHTML('beforeend', '<li class="is-empty">「あとで読む」ページはありません</li>')
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
   }
 }
 
-const createTweetButton = () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    document.getElementById('tweet').setAttribute('href', `https://twitter.com/intent/tweet?url=${tab.url}&text=あとで読む+%2F+${tab.title}`)
-  })
-}
-
-const openNewTab = (e) => {
-  e.preventDefault()
-
-  const id = e.currentTarget.children[0].dataset.id
-  const url = e.currentTarget.children[0].getAttribute('href')
-
-  window.open(url)
-
-  deleteDataStorage(id)
-}
-
-const uniqueId = () => {
-  function s4 () {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
-}
+new Atoyomi()
